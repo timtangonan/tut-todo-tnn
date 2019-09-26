42fca8d3158c3802367a210ceae4dd6fdd5c9407
Tim Tangonan <trstangonan@gmail.com>
2019-09-26 20:24:21 +0800

    finished core functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
+    state = {
+        todos: [
+            { id: 1, content: 'buy some milk' },
+            { id: 2, content: 'play Monster Hunter' }
+        ]
+    }
+    deleteTodo = (id) => {
+        const todos = this.state.todos.filter(todo => {
+            return todo.id !== id
+        });
+        this.setState({
+            todos
+        })
+    }
+    addTodo = (todo) => {
+        todo.id = Math.random();
+        let todos = [...this.state.todos, todo]
+        this.setState({
+            todos
+        })
+    }
     render(){
         return (
-            <div className="App">
-
+            <div className="todo-app container">
+                <h1 className="center blue-text">Todo List</h1>
+                <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+                <AddTodo addTodo={this.addTodo}/>
             </div>
         );
     }
